10cd7f0d79d8b863e5e7fb85ce21c69b3ba6b2c3
Иван Валеев <dp171190vii1@gmail.com>
2020-06-20 19:18:37 +0300

    Create slack_project

---
+++ dialogs.py
@@ -0,0 +1,8 @@
+dialog_add_table = {
+    "title": "Add table to Trello",
+    "submit_label": "Send",
+    "callback_id" : "12345678",
+    "elements" : [{"type" : "text",
+                   "label": "Type name of table",
+                   "name":"name of board"}]
+    }
